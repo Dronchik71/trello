fa4c4edbc83b292e5e43caaea4d548e94baa9c3c
Dronchik71 <72738451+Dronchik71@users.noreply.github.com>
2021-01-28 19:28:00 +0300

    trello
    trello

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function App() {
+  return (
+    
+    <div className="container" align="center">
+     <div><p align="left">Время</p></div>
+
+     <div> <p align="left">Всего задач</p> <p align="right">Выполненые задачи</p> </div>
+    
+     <div className="task"> <ul> 1 </ul> </div>
+
+     <div className="basement"> <p align="right"><img src="img/plus.png"></img> </p> </div>
+
+
+   </div>
+   
+  )
+}
+
+export default App;
